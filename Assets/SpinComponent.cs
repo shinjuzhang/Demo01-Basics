722aee5f4b699edb26ddada81b1fc7abf6590eb0
shinjuzhang <69443307+shinjuzhang@users.noreply.github.com>
2020-12-23 15:46:23 +0900

    Rotate cube
    Added code to rotate the cube

--- Assets/SpinComponent.cs
+++ Assets/SpinComponent.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class SpinComponent : MonoBehaviour
 {
     private bool printedMessage;
+    Transform myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello from Start");
+        myTransform = GetComponent<Transform>();
+        Debug.Log(myTransform.position);
+        Debug.Log(myTransform.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+      
+        myTransform.Rotate(0.1f, 0f, 0f);
+
 
         if (!printedMessage)
         {
