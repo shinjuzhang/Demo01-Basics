9ec74d8df45c76d6c9032ff701bf1e2ff00ccd79
shinjuzhang <69443307+shinjuzhang@users.noreply.github.com>
2020-12-23 13:11:29 +0900

    Added C# script and updated method
    Added the C# script and changed the updated method to only print out once.

---
+++ Assets/SpinComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinComponent : MonoBehaviour
+{
+    private bool printedMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!printedMessage)
+        {
+            Debug.Log("Hello from Update");
+            printedMessage = true;
+
+        }
+
+
+    }
+}
